5a1f9e3f112020a534fdfe825b0803196d1364e3
jasonp000 <76072597+jasonp000@users.noreply.github.com>
2020-12-17 11:16:14 -0500

    Update variables.tf

--- app-web/variables.tf
+++ app-web/variables.tf
@@ -2,6 +2,4 @@ variable "ami" {}
 variable "web_count" {}
 variable "subnet_id" {}
 variable "identity" {}
-variable "vpc_security_group_ids" {
-  type = list
-}
+variable "vpc_security_group_ids" {}
